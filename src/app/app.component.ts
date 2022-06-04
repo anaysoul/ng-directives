49ae5e838c25add7c604d2e7637d58e6caedf0ff
anaysoul <soulyana_lakew@outlook.com>
2022-06-04 14:19:05 -0400

    getting some data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,37 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'pages';
+  images = [
+    {
+      title: 'At the Beach',
+      url: 'https://unsplash.com/photos/Ox_6M68VqfQ',
+    },
+    {
+      title: 'At the Forest',
+      url: 'https://unsplash.com/photos/-heLWtuAN3c',
+    },
+    {
+      title: 'At the Mountain',
+      url: 'https://unsplash.com/photos/9wg5jCEPBsw',
+    },
+    {
+      title: 'At the Volcano',
+      url: 'https://unsplash.com/photos/tLxGw_ITs7k',
+    },
+    {
+      title: 'At the River',
+      url: 'https://unsplash.com/photos/M1YdS0g8SRA',
+    },
+    {
+      title: 'At the Grassland',
+      url: 'https://unsplash.com/photos/bNOh-Y-ECSg',
+    },
+    {
+      title: 'At the Jungle',
+      url: 'https://unsplash.com/photos/qLW70Aoo8BE',
+    },
+  ];
 }
